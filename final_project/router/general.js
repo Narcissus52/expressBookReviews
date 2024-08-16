abd9baceec621476999c4b824f5e4f711d2cd1b7
Obsidian Narcissus <111918789+Narcissus52@users.noreply.github.com>
2024-08-16 16:13:20 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,45 +6,62 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(username);
+    console.log(password);
+    console.log(req.body);
+    return res.status(400).json({ error: "sd" });
+  if (!username && username.length > 1)
+    return res.status(400).json({ error: "name is required" });
+  if (!password &&  password.length > 1)
+    return res.status(400).json({error: "password is required"});
 
-  if (req.body.email) {
-      users[req.body.email] = {
-          "firstName": req.body.firstName,
-          "lastName": req.body.lastNameName,
-          "dateOfBirth": req.body.DOB,
-      };
-  }
-  // Send response indicating user addition
-  res.send("The user" + (' ') + (req.body.firstName) + " Has been added!");
+  // check if username is already exist or not
+  //if (!isValid(username))
+  //  return res.status(400).json({ error: "user is already exist" });
+
+  users.push({username: username,password: password,});
+  res.status(200).json({ message: "user registered successfully.", users });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+  //Write your code here
+  res.send(JSON.stringify({ books }, null, 4));
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]);
-});
+    const book_need = req.params.isbn;
+    res.send(books[book_need]);
+    
+ });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    res.send(books[author]);
+    const authors = req.params.author;
+    const book_arrs =  Object.values(books);
+  
+    const book = book_arrs.filter((book) => book.author === authors);
+    res.status(200).json(book);
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    res.send(books[title]);
+    const book_arr = Object.values(books);
+    const book = book_arr.filter((book) => book.title === title);
+    res.status(200).json(book);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    const review = req.params.review;
-    res.send(books[review]);
+    const book_isbn = req.params.isbn;
+    const book = books[book_isbn];
+    res.send(book.reviews);
 });
 
 module.exports.general = public_users;
